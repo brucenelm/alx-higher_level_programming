0f52385b6e74da33f605077039279757975effa8
brucenelm <brucenelm@gmail.com>
2021-11-18 13:36:41 -0800

    created 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    sum_arg = 0
+    for i in range(len(sys.argv) - 1):
+        sum_arg += int(sys.argv[i + 1])
+    print("{}".format(sum_arg))
